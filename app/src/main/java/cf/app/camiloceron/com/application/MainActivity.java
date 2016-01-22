53f8ea619b484e5e2928f80967094eeb24fe8e88
CamiloCeronGomez <camiloceron1990@gmail.com>
2016-01-22 16:03:33 -0500

    Video 11 Caracteristicas Botones

--- app/src/main/java/cf/app/camiloceron/com/application/MainActivity.java
+++ app/src/main/java/cf/app/camiloceron/com/application/MainActivity.java
@@ -2,32 +2,60 @@ package cf.app.camiloceron.com.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 // declaro una variable de texto
-    TextView campo;
+    TextView campoN;
+    TextView campoC;
     EditText Usr;
     EditText Pass;
+    Button cambioUno;
+    Button cambioDos;
 // declaro una variable de iamgen
     ImageView Img;
+    ImageView Img2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // a la variable llamada campo le voy a colocar lo que este en txtView
-        // campo = (TextView)findViewById(R.id.txtView);
+         campoN = (TextView)findViewById(R.id.txtUsuario);
+        campoC = (TextView)findViewById(R.id.txtContraseña);
         //Le voy a enviar a campo lo que esta esfrito en el parentesis Hola Camilo Ceron
         // campo.setText("Hola Camilo Ceron");
         Usr = (EditText)findViewById(R.id.edtUsuario);
-        Usr.setText("Colocar Usuario");
+        // Usr.setText("Colocar Usuario");
         Pass = (EditText)findViewById(R.id.edtContraseña);
-        Pass.setText("Colocar Contraseña");
+       // Pass.setText("Colocar Contraseña");
         // Asigno la variable a la respectiva representacion grafica
         Img = (ImageView)findViewById(R.id.ImgUno);
+        Img2 = (ImageView)findViewById(R.id.ImgUno);
+        cambioUno = (Button)findViewById(R.id.btnUno);
+        cambioDos = (Button)findViewById(R.id.btnDos);
+// habilito los botones para que funcionen en el contexto this
+        cambioUno.setOnClickListener(this);
+        cambioDos.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnUno:
+                String dato1 = Usr.getText().toString();
+                campoN.setText(dato1);
+                String dato2 = Pass.getText().toString();
+                campoC.setText(dato2);
+                break;
+            case  R.id.btnDos:
+              Img2.setImageResource(R.drawable.sinfoto);
 
 
+        }
     }
 }
