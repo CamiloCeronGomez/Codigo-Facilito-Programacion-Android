9ff888ff17ca3d08c8c7e127df354bee69e54338
CamiloCeronGomez <camiloceron1990@gmail.com>
2016-01-21 19:41:27 -0500

    Video 9 caracteristicas EditText

--- app/src/main/java/cf/app/camiloceron/com/application/MainActivity.java
+++ app/src/main/java/cf/app/camiloceron/com/application/MainActivity.java
@@ -2,20 +2,27 @@ package cf.app.camiloceron.com.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 // declaro una variable de texto
     TextView campo;
+    EditText Usr;
+    EditText Pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // a la variable llamada campo le voy a colocar lo que este en txtView
-        campo = (TextView)findViewById(R.id.txtView);
+       // campo = (TextView)findViewById(R.id.txtView);
         //Le voy a enviar a campo lo que esta esfrito en el parentesis Hola Camilo Ceron
-        campo.setText("Hola Camilo Ceron");
+       // campo.setText("Hola Camilo Ceron");
+        Usr = (EditText)findViewById(R.id.edtUsuario);
+        Usr.setText("Colocar Usuario");
+        Pass = (EditText)findViewById(R.id.edtContraseña);
+        Pass.setText("Colocar Contraseña");
 
     }
 }
